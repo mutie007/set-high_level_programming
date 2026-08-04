74f82cf20120cb7650f49fb7a5429ca3b9d46f94
mutie007 <your@email.com>
2026-08-04 21:58:49 +0300

    Add insert_node function for sorted linked list

--- python-if_else_loops_functions/13-insert_number.c
+++ python-if_else_loops_functions/13-insert_number.c
@@ -3,10 +3,10 @@
 
 /**
  * insert_node - inserts a number into a sorted singly linked list
- * @head: pointer to pointer of the first node
- * @number: the number to insert
+ * @head: pointer to pointer of the first node of the list
+ * @number: integer to be included in the new node
  *
- * Return: the address of the new node, or NULL if it failed
+ * Return: address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
