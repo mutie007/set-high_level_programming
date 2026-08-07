b50e01bb4010f9abdda81b59fcbe75d6508ccaea
mutie007 <your@email.com>
2026-08-07 17:51:00 +0300

    Fix pycodestyle E127 in 102-complex_delete.py

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -4,8 +4,9 @@
 
 def complex_delete(a_dictionary, value):
     """Deletes all keys in a_dictionary that have the given value."""
-    keys_to_delete = [key for key in a_dictionary
-                       if a_dictionary[key] == value]
+    keys_to_delete = [
+        key for key in a_dictionary if a_dictionary[key] == value
+    ]
     for key in keys_to_delete:
         del a_dictionary[key]
     return a_dictionary
