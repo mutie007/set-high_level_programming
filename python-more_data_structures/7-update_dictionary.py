ac291ddaeae49172b52dc450d4b06c31cdff128f
mutie007 <your@email.com>
2026-08-07 17:30:04 +0300

    Add 7-update_dictionary.py

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines update_dictionary."""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Replaces or adds a key/value pair in a_dictionary."""
+    a_dictionary[key] = value
+    return a_dictionary
