426814348e3eeb2f5337b8e40f94376f87133549
mutie007 <your@email.com>
2026-08-20 17:59:05 +0300

    Add from_json_string static method to Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -55,3 +55,18 @@ class Base:
             list_dicts = [obj.to_dictionary() for obj in list_objs]
         with open(filename, "w") as f:
             f.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representation json_string.
+
+        Args:
+            json_string (str): A string representing a list of dictionaries.
+
+        Returns:
+            list: The list represented by json_string, or [] if None/empty.
+        """
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
