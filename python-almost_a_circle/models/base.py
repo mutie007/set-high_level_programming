d70e7e14331535073f920ad3fab13fd26aef0414
mutie007 <your@email.com>
2026-08-20 18:00:41 +0300

    Add create class method to Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -70,3 +70,21 @@ class Base:
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Returns an instance with all attributes already set.
+
+        Args:
+            **dictionary: Double pointer to a dictionary of attributes.
+
+        Returns:
+            An instance of cls with attributes set from dictionary.
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
