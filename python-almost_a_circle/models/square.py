a76e380ae0deecb76391090960bc6f7ca2592d94
mutie007 <your@email.com>
2026-08-20 17:46:30 +0300

    Add Square class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,41 +1,33 @@
 #!/usr/bin/python3
-"""Square class module"""
+"""
+This module defines the Square class that inherits from Rectangle.
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class"""
+    """
+    Square class that inherits from Rectangle.
+    """
 
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square (width and height).
+            x (int): The x coordinate. Defaults to 0.
+            y (int): The y coordinate. Defaults to 0.
+            id (int): The identity of the instance. Defaults to None.
+        """
         super().__init__(size, size, x, y, id)
 
-    @property
-    def size(self):
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        self.width = value
-        self.height = value
-
     def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+        """
+        Returns the string representation of the Square.
 
-    def update(self, *args, **kwargs):
-        if args:
-            attrs = ["id", "size", "x", "y"]
-            for i, arg in enumerate(args):
-                if i < len(attrs):
-                    setattr(self, attrs[i], arg)
-        else:
-            for key, value in kwargs.items():
-                if hasattr(self, key):
-                    setattr(self, key, value)
-
-    def to_dictionary(self):
-        return {
-            "id": self.id,
-            "size": self.size,
-            "x": self.x,
-            "y": self.y
-        }
+        Returns:
+            str: [Square] (<id>) <x>/<y> - <size>
+        """
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
