55121a54709663d61def96fc0be99b5a3bcb5de8
mutie007 <your@email.com>
2026-08-20 21:01:26 +0300

    Fix 0-select_states.py + add README

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,17 +7,24 @@ import sys
 
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database
     )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
-    cur.close()
+
+    cursor.close()
     db.close()
---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,9 @@
+# 0x0F. Python - Object-relational mapping
+
+## Description
+This project is about Object-Relational Mapping (ORM) using MySQLdb and SQLAlchemy.
+
+## Files
+| File | Description |
+|------|-------------|
+| `0-select_states.py` | Lists all states from the database |
