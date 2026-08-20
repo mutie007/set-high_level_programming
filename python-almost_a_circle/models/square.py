59b8773c3e22f16ffeefa5e402e49951cffd260e
mutie007 <your@email.com>
2026-08-20 17:48:02 +0300

    Add size property to Square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -22,6 +22,17 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square (assigns both width and height)."""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """
         Returns the string representation of the Square.
