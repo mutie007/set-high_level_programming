fac238f396243299b6c28fddb792312a44d7cf3c
mutie007 <your@email.com>
2026-08-21 16:02:05 +0300

    Make state name comparison case-sensitive

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+    query = "SELECT * FROM states WHERE name = BINARY '{}' ORDER BY id ASC".format(
         state_name
     )
     cursor.execute(query)
