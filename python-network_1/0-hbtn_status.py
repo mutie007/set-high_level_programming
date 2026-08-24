7a7c14c6b3bd1751feb06bc51a7612c37fbf76f5
mutie007 <your@email.com>
2026-08-24 10:07:31 +0300

    Fix documentation and style for status scripts

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Script that fetches https://alx-intranet.hbtn.io/status using urllib"""
+"""Fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Script that fetches https://alx-intranet.hbtn.io/status using requests"""
+"""Fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
-
-r = requests.get('https://alx-intranet.hbtn.io/status')
-print("Body response:")
-print("\t- type: {}".format(type(r.text)))
-print("\t- content: {}".format(r.text))
+if __name__ == "__main__":
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
