d49651edde1ebd92ad9a8f1dfb6a48ab35d27fde
mutie007 <your@email.com>
2026-08-24 09:29:46 +0300

    Try intranet.hbtn.io URL

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status"""
+"""Fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
