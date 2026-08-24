54a0d748134125c486b5ae2fd6797568e068f1d1
mutie007 <your@email.com>
2026-08-24 09:41:15 +0300

    Add 5-hbtn_header.py

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Displays the X-Request-Id header variable using requests"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
