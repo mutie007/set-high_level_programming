652a752654e2a8add81c761e2504e379595e99ad
mutie007 <your@email.com>
2026-08-24 10:05:41 +0300

    Fix 0 and 4 hbtn_status scripts

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status"""
+"""Script that fetches https://alx-intranet.hbtn.io/status using requests"""
 import requests
 
-if __name__ == "__main__":
-    response = requests.get('https://alx-intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+
+r = requests.get('https://alx-intranet.hbtn.io/status')
+print("Body response:")
+print("\t- type: {}".format(type(r.text)))
+print("\t- content: {}".format(r.text))
