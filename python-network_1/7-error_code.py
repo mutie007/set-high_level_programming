c23ec54dc19cada19d882c8c55abec5412f42138
mutie007 <your@email.com>
2026-08-24 09:43:45 +0300

    Add 7-error_code.py

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Sends a request and displays the body or error code using requests"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
